9d6c0249c0827deb2175dfe4840541189dd4987c
Bjonne <Bjonne696@gmail.com>
2024-02-24 22:14:48 +0100

    create venue works, but wont display

--- src/hooks/venuesService.jsx
+++ src/hooks/venuesService.jsx
@@ -1,4 +1,4 @@
-// project-exam-2-holidaze/src/hooks/venuesService.jsx
+// project-exam-2-holidaze/src/hooks/venuesService.js
 
 import BASE_URL from '../constants/api';
 
@@ -11,6 +11,8 @@ export const createVenue = async (venueData, token) => {
     },
     body: JSON.stringify(venueData),
   });
-  if (!response.ok) throw new Error('Failed to create venue');
+  if (!response.ok) {
+    throw new Error(`Failed to create venue, status: ${response.status}`);
+  }
   return response.json();
 };
